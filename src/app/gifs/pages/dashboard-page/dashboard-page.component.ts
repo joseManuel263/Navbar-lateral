711e3b40c9176daf121377084b683154aebb9392
joseManuel263 <conoplant2@gmail.com>
2025-10-13 07:54:37 -0600

    subir cambios

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarlateralComponent } from '../../pages/navbarlateral/navbarlateral.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NavbarlateralComponent],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss'
+})
+export default class DashboardComponent {
+
+}
---
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  imports: [],
+  templateUrl: './trending-page.component.html',
+  styleUrl: './trending-page.component.scss'
+})
+export class TrendingPageComponent {
+
+}
