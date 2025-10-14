311fe4022b3558a9fbcb43be9cc0d6d2aad99150
joseManuel263 <conoplant2@gmail.com>
2025-10-14 07:19:04 -0600

    solucion de errores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SettingsComponent } from './settings/settings.component';
-import { DatabaseComponent } from './database/database.component';
-import { ReportsComponent } from './reports/reports.component';
-import { CloudstorageComponent } from './cloudstorage/cloudstorage.component';
-import { SecurityComponent } from './security/security.component';
-import { ExtencionsComponent } from './extencions/extencions.component';
-import { NavbarlateralComponent } from './navbarlateral/navbarlateral.component';
+import DashboardComponent from './gifs/pages/dashboard-page/dashboard-page.component';
+import { SettingsComponent } from './gifs/components/settings/settings.component';
+import { DatabaseComponent } from './gifs/components/database/database.component';
+import { ReportsComponent } from './gifs/components/reports/reports.component';
+import { CloudstorageComponent } from './gifs/components/cloudstorage/cloudstorage.component';
+import { SecurityComponent } from './gifs/components/security/security.component';
+import { ExtencionsComponent } from './gifs/components/extencions/extencions.component';
+import { NavbarlateralComponent } from './gifs/pages/navbarlateral/navbarlateral.component';
+import { SearchPageComponent } from './gifs/pages/search-page/search-page.component';
+import { TrendingPageComponent } from './gifs/pages/trending-page/trending-page.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
@@ -17,6 +19,8 @@ export const routes: Routes = [
     { path: 'extensions', component: ExtencionsComponent },
     { path: 'settings', component: SettingsComponent },
     { path: 'navbarlateral', component: NavbarlateralComponent },
+    { path: 'search', component: SearchPageComponent },
+    { path: 'trending', component: TrendingPageComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: '**', redirectTo: '/dashboard' }
 ];
